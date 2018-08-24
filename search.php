4aa8ba68da6a7e50aa5b9b9bd7aab6e92723084c
root <root@localhost.localdomain>
2018-08-24 15:02:34 +0800

    Change root dir

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <?php
 
-$root = '/sharedisk/Dep_Content';
+$root = '/home/share/Dep_Content';
 $target = $root.$_POST['filename'];
 
 $info = shell_exec('ffprobe -v error -show_entries stream=codec_name,codec_type,avg_frame_rate,width,height,display_aspect_ratio:format=format_name,bit_rate "'.$target.'"');
